af06b05b28f90e8a3043653a000726d8931c7c49
Kiel Stirling <kielstirling@gmail.com>
2017-11-30 20:35:13 +1100

    remove some files

--- docker/container_builder/svn_checkout/selenium/discover.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import xmlrunner
-
-__unittest = True
-
-from unittest.main import main, TestProgram, USAGE_AS_MAIN
-TestProgram.USAGE = USAGE_AS_MAIN
-
-main(module=None, verbosity=2, testRunner=xmlrunner.XMLTestRunner(output='test-results'), failfast=False, buffer=False, catchbreak=False)
--- docker/container_builder/tmp/z4py37UUNU/default/html/js/accordion-script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  
-  $("#accordion").accordion({
-  			collapsible: true,
-  			active: false,
-  			autoHeight: false
-  		});
-
-});
