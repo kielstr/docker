9ab85d82642740e88a35c6766b07196c2f15e531
Kiel Stirling <kielstirling@gmail.com>
2017-12-02 12:37:48 +1100

    adding files

---
+++ create-swarm.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+
+REGEX='^node-[0-9]{1,}$'
+
+for m in $(docker-machine ls -q); do 
+    if [[ $m =~ $REGEX ]] 
+    then 
+
+        echo "${m} exists remove it? [y/n] "
+
+        read response
+
+        if [ "$(echo "$response" | tr '[:upper:]' '[:lower:]')" == "y" ] 
+            then 
+            docker-machine rm -y ${m}
+        fi
+    fi
+done;
+
+
+for i in 1 2 3; do
+  docker-machine create --engine-insecure-registry $DOCKER_REGISTORY -d virtualbox node-$i
+done
+
+./bin/docker-machine-ipconfig.sh static node-1 192.168.99.100
+./bin/docker-machine-ipconfig.sh static node-2 192.168.99.101
+./bin/docker-machine-ipconfig.sh static node-3 192.168.99.102
+
+eval $(docker-machine env node-1)
+
+docker swarm init --advertise-addr $(docker-machine ip node-1)
+
+TOKEN=$(docker swarm join-token -q worker)
+
+for i in 2 3; do
+  eval $(docker-machine env node-$i)
+  docker swarm join --token $TOKEN $(docker-machine ip node-1):2377
+done
+
+echo "Swarm cluster has been successfuly created !";
+
+eval $(docker-machine env node-1)
+
+docker node ls
